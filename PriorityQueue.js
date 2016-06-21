48af82b97f8286752f6134bebc4b6ba47cddf75e
fsk310 <fsk310@hotmail.com>
2016-06-21 16:43:52 +0800

    学习javascript数据结构和算法

---
+++ PriorityQueue.js
@@ -0,0 +1,45 @@
+function PriorityQueue() {
+    var items = [];
+    function QueueElement (element, priority){ // {1}
+        this.element = element;
+        this.priority = priority;
+    }
+    this.enqueue = function(element, priority){
+        var queueElement = new QueueElement(element, priority);
+        if (this.isEmpty()){
+            items.push(queueElement); // {2}
+        } else {
+            var added = false;
+            for (var i=0; i<items.length; i++){
+                if (queueElement.priority < items[i].priority){
+                    items.splice(i,0,queueElement); // {3}
+                    added = true;
+                    break; // {4}
+                }
+            }
+            if (!added){ //{5}
+                items.push(queueElement);
+            }
+        }
+    };
+
+    //其他方法和默认的Queue实现相同
+    this.dequeue = function(){
+        return items.shift();
+    };
+    this.front = function(){
+        return items[0];
+    };
+    this.isEmpty = function(){
+        return items.length == 0;
+    };
+    this.clear = function(){
+        items = [];
+    };
+    this.size = function(){
+        return items.length;
+    };
+    this.print = function(){
+        console.log(items.toString());
+    };
+}
