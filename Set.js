ba79d8b7ce24a705d5d3c521c7352168d5aedc0f
fsk310 <fsk310@hotmail.com>
2016-06-24 15:56:48 +0800

    数据结构与算法

---
+++ Set.js
@@ -0,0 +1,27 @@
+function Set() {
+    var items = {};
+
+    this.has = function(value) {
+        return items.hasOwnProperty(value);
+    };
+
+    this.add = function(value) {
+        if (!this.has(value)) {
+            items[value] = value;
+            return true;
+        }
+        return false;
+    };
+
+    this.remove = function(value) {
+        if (this.has(value)) {
+            delete items[value];
+            return true;
+        }
+        return false;
+    };
+
+    this.clear = function() {
+        items = {};
+    };
+}
